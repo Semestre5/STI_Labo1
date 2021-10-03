17f727ea0e2f0a3b96fec427e81728f5a9276f48
Axel Vallon <axel.vallon123@gmail.com>
2021-10-03 22:49:28 +0200

    avancement majeur graphisme

--- www/html/index.php
+++ www/html/index.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+    <title>Messagerie</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js" ></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Custom styles for this template -->
+    <link href="css/signing.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    <form class="form-signin">
+        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label for="inputEmail" class="sr-only">Login</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Login" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+</form>
 </body>
 </html>
---
+++ www/html/messagerie.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Messagerie</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js" ></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signing.css" rel="stylesheet">
+</head>
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th scope="col">Login</th>
+        <th scope="col">Date reception</th>
+        <th scope="col">Sujet</th>
+        <th scope="col"></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>AxelVallon</td>
+        <td>03.10.2021</td>
+        <td>Labo STI</td>
+        <td>
+            <a class="btn btn-success" href="#" role="button">Répondre</a>
+            <a class="btn btn-info" href="#" role="button">Détails</a>
+            <a class="btn btn-warning" href="#" role="button">Refuser</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+    <!-- https://github.com/CapitainMorgan/ProjetBDR/blob/main/src/php/vueOffreEmploi.php -->
+<?php
+
